e3fc887f4e5b362ead56863123b68912e2fe56ca
EdumPass <150535111+EdumPass@users.noreply.github.com>
2023-12-07 16:42:55 +0530

    Update serializers.py

--- Edumbass_backend/projects/api/serializers.py
+++ Edumbass_backend/projects/api/serializers.py
@@ -6,7 +6,7 @@ from ..models import Project
 class ProjectListSerializer(ModelSerializer):
     class Meta:
         model = Project
-        fields = ('id', 'title','slug','short_description', 'get_image')
+        fields = ('id', 'title','slug','short_description', 'get_image','long_description','youtube_id')
 
 class ProjectDetailSerializer(ModelSerializer):
     class Meta:
